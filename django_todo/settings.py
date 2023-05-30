1bff7fb38f02adc61995da97b0657c7831ea5606
sonetto104 <peter_regan88@yahoo.com>
2023-05-30 14:24:46 +0000

    Remove secret key from source as this app is not using one.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
