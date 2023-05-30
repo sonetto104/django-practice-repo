ba281f24a941e971c0d70c894dd5ff9af1b0f92c
sonetto104 <peter_regan88@yahoo.com>
2023-05-30 15:50:33 +0000

    change heroku allowed host back to previous

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOST')]
+ALLOWED_HOSTS = ['django-taskmanager-app.herokuapp.com']
 
 
 # Application definition
